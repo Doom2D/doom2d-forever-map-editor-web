8b881d2278c573d6f3b3efe274d3fe3b1b9a0997
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-09-26 18:02:57 +0300

    add basic api

---
+++ src/editor/api/api.ts
@@ -0,0 +1,8 @@
+import parseFalcon from '../../parser/falcon/typescript-falcon'
+import parseKetmar from '../../parser/ketmar/typescript-ketmar'
+
+function loadMap(src: string) {
+  return parseKetmar(src)
+}
+
+export { loadMap }
