449d7ab6bb85cc0bc7941b75a7183b2814cb7b54
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-01 18:17:50 +0300

    update ResourceArchive interface

--- src/df/resource/wad/interface.ts
+++ src/df/resource/wad/interface.ts
@@ -1,5 +1,7 @@
+import type ResourcePath from '../path/path'
+
 interface ResourceArchive {
-  getFiles: () => string[]
+  getFiles: () => ResourcePath[]
   loadFileAsArrayBuffer: (f: string) => ArrayBuffer
   loadFileAsString: (f: string) => string
   saveFileArrayBuffer: (path: string, content: Readonly<ArrayBuffer>) => void
