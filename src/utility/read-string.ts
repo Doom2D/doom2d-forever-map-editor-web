5f90fd0f928c2d0c118ea33648c4975be49a464a
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-02 11:02:32 +0300

    modify read-string so it reads the whole buffer if len is undefined

--- src/utility/read-string.ts
+++ src/utility/read-string.ts
@@ -1,11 +1,12 @@
 export default function readString(
   buffer: Readonly<ArrayBuffer>,
   start: number,
-  len: number,
+  len: number | undefined,
   decode: 'utf8' | 'win1251'
 ) {
-  const slice = new Uint8Array(buffer)
-    .slice(start, start + len)
+  const view = new Uint8Array(buffer)
+  const slice = view
+    .slice(start, start + (len ?? view.length))
     .filter((element) => element !== 0)
   // eslint-disable-next-line unicorn/text-encoding-identifier-case
   let enc = new TextDecoder('utf-8')
