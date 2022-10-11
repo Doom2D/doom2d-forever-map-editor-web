c13c1fc9f6de4ff001ccd4053644bd910355f640
Maksim Brylov <maksim.brylov@gmail.com>
2022-10-11 22:49:29 +0300

    remove DFWadFrom and add init() to DFWad

--- src/df/resource/wad/dfwad.ts
+++ src/df/resource/wad/dfwad.ts
@@ -1,6 +1,20 @@
-import type ResourcePath from '../path/path'
+import { inflate } from 'pako'
+
+import {
+  loadZipObject,
+  loadFileFromZip,
+} from '../../../third-party/jszip/wrapper'
+import isBufferBinary from '../../../utility/is-buffer-binary'
+import readString from '../../../utility/read-string'
+import pathSplit from '../../../utility/split-path'
+import ResourcePath from '../path/path'
+import {
+  ResourcePathFromWad,
+  ResourcePathFromZipObject,
+} from '../path/path-from'
 
 import type ResourceArchive from './interface'
+import { wadAsJson } from './wad-as-json'
 
 class DFWad implements ResourceArchive {
   public type: 'dfwad' | 'none' | 'zip' = 'none'
@@ -21,6 +35,94 @@ class DFWad implements ResourceArchive {
   public saveFileArrayBuffer = () => true
 
   public saveFileString = () => true
+
+  public constructor(private readonly src: Readonly<ArrayBuffer>, private readonly name = '') {}
+
+  public async init() {
+    const tryWad = wadAsJson(this.src)
+    if (tryWad.isValid) {
+      this.isSupported = true
+      for (const [, value] of Object.entries(tryWad.wadObject)) {
+        if (value.type === 'parent') {
+          continue
+        }
+        const buffer = this.src.slice(
+          value.memAddress,
+          value.memAddress + value.memLength
+        )
+        const content = inflate(buffer)
+        const path = ResourcePathFromWad(value, this.name)
+        this.getFiles = () => this.getFiles().concat(path)
+        if (!isBufferBinary(content.buffer)) {
+          this.files = this.files.concat({
+            path,
+            content: readString(content.buffer, 0, undefined, 'win1251'),
+          })
+        } else {
+          this.getFiles = () => this.getFiles().concat(path)
+          this.files = this.files.concat({
+            path,
+            content,
+          })
+        }
+      }
+      this.type = 'dfwad'
+    } else {
+      try {
+        const zip = await loadZipObject(this.src)
+        this.isSupported = true
+        const promises: Promise<boolean>[] = []
+        for (const [key] of Object.entries(zip.files)) {
+          const handleEntry = async () => {
+            const path = ResourcePathFromZipObject(key, this.name)
+            this.getFiles = () => this.getFiles().concat(path)
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+            const content = (await loadFileFromZip(
+              zip,
+              key,
+              'uint8array'
+            )) as Uint8Array
+            if (isBufferBinary(content.buffer)) {
+              this.files = this.files.concat({
+                path,
+                content,
+              })
+            } else {
+              // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+              const contentString = (await loadFileFromZip(
+                zip,
+                key,
+                'string'
+              )) as string
+              this.files = this.files.concat({
+                path,
+                content: contentString,
+              })
+            }
+            return true
+          }
+          promises.push(handleEntry())
+        }
+        await Promise.all(promises)
+        this.type = 'zip'
+      } catch {
+        this.isSupported = false
+      }
+    }
+    if (this.isSupported) {
+      this.loadFileAsString = (f: string) => {
+        const split = pathSplit(f)
+        const path = new ResourcePath(split.directories, split.fileName, '')
+        const v = this.files
+          .filter((q) => q.path.getBaseName() === path.getBaseName())
+          .pop()
+        if (!v) return ''
+        if (typeof v.content === 'string') return v.content
+        return readString(v.content, 0, undefined, 'win1251')
+      }
+    }
+    return true
+  }
 }
 
 export { DFWad }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { loadMap } from './editor/api/api'
 import { wadAsJson } from './df/resource/wad/wad-as-json'
 import { DFWad } from './df/resource/wad/dfwad'
 import pathSplit from './utility/split-path'
-import DFWadFrom from './df/resource/wad/dfwad-from'
 const n = 30
 
 setInterval(() => {
@@ -15,8 +14,9 @@ render.init(document.body)
 fetch('./CTF_TORMENT2.dfz')
   .then((x) => {
     x.arrayBuffer().then(async (b) => {
-      const t3 = await DFWadFrom(b, pathSplit('./CTF_TORMENT2.dfz').fileName)
-      // console.log(t3)
+      const t3 = new DFWad(b, pathSplit('./CTF_TORMENT2.dfz').fileName)
+      await t3.init()
+      console.log(t3)
       // console.log(t3)
       const o = t3.loadFileAsString('MAP01.txt')
       // console.log(o)
