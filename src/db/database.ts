00a0ca5d5732b37b9847b113192e7225ae0ec5b9
Maksim Brylov <maksim.brylov@gmail.com>
2022-10-13 23:00:15 +0300

    remove erroneous function from Database, leave it outside of the class

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,9 +1,9 @@
 /* eslint-disable promise/avoid-new */
-/* eslint-disable prefer-promise-reject-errors */
 class Database {
-  private readonly store = 'store'
-
-  public constructor(private readonly db: IDBDatabase) {}
+  public constructor(
+    private readonly db: IDBDatabase,
+    private readonly store = 'store'
+  ) {}
 
   private async getItem(
     transaction: IDBTransaction,
@@ -55,29 +55,32 @@ class Database {
     const transaction = this.db.transaction(this.store)
     return await this.addItem(transaction, this.store, v, key)
   }
+}
 
-  public async open() {
-    const load = async () => {
-      const openp = new Promise((resolve, reject) => {
-        const orequest = window.indexedDB.open(this.store, 1)
-        orequest.addEventListener('upgradeneeded', () => {
-          const db = orequest.result
-          db.createObjectStore('store')
-        })
-        orequest.addEventListener('blocked', () => {
-          reject('Error creating Database!')
-        })
-        orequest.addEventListener('error', () => {
-          reject('Error creating Database!')
-        })
-        orequest.addEventListener('success', () => {
-          resolve(true)
-        })
+async function openIDB(store = 'store') {
+  const load = async () => {
+    const openp = new Promise<IDBDatabase>((resolve, reject) => {
+      const orequest = window.indexedDB.open(store, 1)
+      orequest.addEventListener('upgradeneeded', () => {
+        const db = orequest.result
+        db.createObjectStore('store')
       })
-      await openp
-    }
-    await load()
+      orequest.addEventListener('blocked', () => {
+        reject(new Error('Error creating Database!'))
+      })
+      orequest.addEventListener('error', () => {
+        reject(new Error('Error creating Database!'))
+      })
+      orequest.addEventListener('success', () => {
+        const db = orequest.result
+        resolve(db)
+      })
+    })
+    return await openp
   }
+  return await load()
 }
 
 export default Database
+
+export { openIDB, Database }
