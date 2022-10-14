0bb883df740063f399039142b65699157e9db485
Maksim Brylov <maksim.brylov@gmail.com>
2022-10-14 20:33:42 +0300

    update isObject utility module to be a type denouncer

--- src/utility/is-object.ts
+++ src/utility/is-object.ts
@@ -1,3 +1,3 @@
-export default function isObject(n: unknown) {
+export default function isObject(n: unknown): n is Record<string, unknown> {
   return typeof n === 'object' && !Array.isArray(n) && n !== null
 }
