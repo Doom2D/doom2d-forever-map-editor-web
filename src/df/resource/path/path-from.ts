5006101d6e62a042f80116a0ec668ff3c1a68e35
Maksim Brylov <maksim.brylov@gmail.com>
2022-10-14 18:15:17 +0300

    add ResourcePath creation from df game path

--- src/df/resource/path/path-from.ts
+++ src/df/resource/path/path-from.ts
@@ -12,4 +12,13 @@ function ResourcePathFromZipObject(k: string, name: string) {
   return new ResourcePath(path.directories, path.fileName, name)
 }
 
-export { ResourcePathFromWad, ResourcePathFromZipObject }
+function ResourcePathFromGamePath(str: string) {
+  const s1 = str.split(':')
+  let source = ''
+  if (s1.length > 1) source = s1[0]
+  const s2 = (s1.pop() ?? str).split('\\')
+  const basename = s2.pop() ?? str
+  return new ResourcePath(s2, basename, source)
+}
+
+export { ResourcePathFromWad, ResourcePathFromZipObject, ResourcePathFromGamePath }
