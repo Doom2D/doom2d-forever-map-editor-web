cd8013665ed70015bda36500d7e7ae6fdce6194e
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-19 18:25:56 +0300

    update example index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
-import parsedMapAsECS from './editor/game/parsed-to-entity'
-import { ecs, render } from './globals'
-import { loadMap } from './editor/api/api'
-import { wadAsJson } from './df/resource/wad/wad-as-json'
 import { DFWad } from './df/resource/wad/dfwad'
+import convertedMap from './editor/game/converted-map'
+import ECSFromMap from './editor/game/map-as-entities'
+import EditorMap from './editor/map/map'
+import FileCategories from './file-category/file-categories'
 import pathSplit from './utility/split-path'
 const n = 30
 
-setInterval(() => {
-  ecs.update()
-}, 1000 / n)
-render.init(document.body)
 
 fetch('./CTF_TORMENT2.dfz')
   .then((x) => {
     x.arrayBuffer().then(async (b) => {
       const t3 = new DFWad(b, pathSplit('./CTF_TORMENT2.dfz').fileName)
       await t3.init()
-      console.log(t3)
       // console.log(t3)
-      const o = t3.loadFileAsString('MAP01.txt')
-      // console.log(o)
-      const q = loadMap(o)
-      // console.log(q)
-      const p = parsedMapAsECS(q, ecs)
-      render.resize(q.size[0], q.size[1])
-      ecs.update()
+      const files = t3.filesForCategorising()
+      const categories = new FileCategories(files)
+      const c = await categories.getCategories()
+      const map = c.filter((x) => x.type === 'map').pop()
+      const content = t3.loadFileWithoutConverting((x) => x === map?.path)
+      if (map === undefined) throw new Error('Shit WAD!')
+      const mapObj = new convertedMap(content)
+      const mapParsed = new EditorMap(mapObj.getUnparsed())
+      console.log(mapParsed)
+      const canvas = document.createElement('canvas')
+      document.body.append(canvas)
+      const ecs = new ECSFromMap(mapParsed, canvas)
+      await ecs.init()
     })
   })
   .catch()
