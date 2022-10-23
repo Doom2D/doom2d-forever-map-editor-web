89753ad6acf7d525a83fb666a3bc901ad5cc72a9
Vasia-Assembler <112320206+Vasia-Assembler@users.noreply.github.com>
2022-10-23 20:07:55 +0300

    sort elements before rendering

--- src/third-party/ecs/system/render.ts
+++ src/third-party/ecs/system/render.ts
@@ -1,8 +1,11 @@
+import type { RenderOptions } from '../../../render/interface'
 import { type Renderer } from '../../../render/interface'
 import ForRender from '../component/for-render'
 import Position from '../component/position'
 import Size from '../component/size'
 import Texture from '../component/texture'
+import Type from '../component/type'
+import PanelType from '../component/panel-type'
 import { type Entity, System } from '../minimal-ecs'
 
 class Render extends System {
@@ -11,6 +14,7 @@ class Render extends System {
     Size,
     Texture,
     ForRender,
+    Type,
   ])
 
   public entitiesLastSeenUpdate = -1
@@ -27,25 +31,43 @@ class Render extends System {
 
   public update(entities: Readonly<Set<Entity>>): void {
     this.entitiesLastSeenUpdate = entities.size
-    entities.forEach((v) => {
+    const renderArray: {
+      ordnung: number
+      opts: RenderOptions
+    }[] = []
+    for (const [, v] of Object.entries(Array.from(entities))) {
       const components = this.ecs.getComponents(v)
       const pos = components.get(Position)
       const size = components.get(Size)
       const texture = components.get(Texture)
       const shouldRender = components.get(ForRender)
+      const type = components.get(Type)
       if (shouldRender.shouldRender()) {
-        this.rendererImplementation.render({
-          x: pos.x,
-          y: pos.y,
-          w: size.w,
-          h: size.h,
-          entity: v,
-          imgKey: texture.key,
-        })
+        if (type.key === 'panel') {
+          const typeComponent = components.get(PanelType)
+          const ordnung = typeComponent.key.giveRenderOrder()
+          renderArray.push({
+            ordnung,
+
+            opts: {
+              x: pos.x,
+              y: pos.y,
+              w: size.w,
+              h: size.h,
+              entity: v,
+              imgKey: texture.key,
+            },
+          })
+        }
       } else {
         this.rendererImplementation.deleteEntity(v)
       }
-    })
+    }
+    for (const [, v] of Object.entries(
+      renderArray.slice().sort((a, b) => a.ordnung - b.ordnung)
+    )) {
+      this.rendererImplementation.render(v.opts)
+    }
   }
 
   public async saveImage(key: string, img: Readonly<HTMLImageElement>) {
